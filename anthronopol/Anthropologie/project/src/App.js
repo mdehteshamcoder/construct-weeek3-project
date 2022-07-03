12ceb7f182c5fbbcf12e787756b9d3c9017d3487
MD EHTESHAM <89984388+mdehteshamcoder@users.noreply.github.com>
2022-07-03 22:28:24 +0530

    Add files via upload

---
+++ anthronopol/Anthropologie/project/src/App.js
@@ -0,0 +1,34 @@
+import SignInSignUp from './components/SignInSignUp';
+import Signup from './components/Signup';
+import './App.css';
+import { useSelector } from 'react-redux';
+import {Routes,Route} from "react-router-dom";
+import MobileSign from './components/MobileSign';
+import MobileOtp from './components/MobileOtp';
+import Home from './components/Home';
+import PaymentPage from './components/PaymentPage';
+import OrderConfirm from './components/OrderConfirm';
+import Footer from './components/Footer';
+function App() {
+
+  const signup = useSelector(state =>state.users);
+  console.log(signup)
+  
+  
+  return (
+    <div className="App">
+      <Footer />
+      <Routes>
+        <Route path = "/" element = {<SignInSignUp />}></Route>
+        <Route path = "/signup" element = {<Signup />}></Route>
+        <Route path = "/mobile" element = {<MobileSign/>} ></Route>
+        <Route path='/mobileotp' element = {<MobileOtp />}></Route>
+        <Route path = "/homepage" element = {<Home/>} ></Route>
+        <Route path='/payment' element = {<PaymentPage />}></Route>
+        <Route path = "/orderconfirm" element = {<OrderConfirm />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
